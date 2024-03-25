e23966c6e7c29ded631e147d6cc4984ab77db7c2
Giovane Barcelos <giovanebarcelos@gmail.com>
2024-03-25 20:19:55 -0300

    feat: menu do cadastro de pessoas

---
+++ src/aula11/MenuApp.java
@@ -0,0 +1,17 @@
+package aula11;
+
+import java.util.Scanner;
+
+public class MenuApp {
+    public int getOpcao() {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("----- Menu ------");
+        System.out.println("1. Incluir");
+        System.out.println("2. Consultar");
+        System.out.println("3. Alterar");
+        System.out.println("4. Excluir");
+        System.out.println("9. Sair");
+        System.out.print("Opção: ");
+        return ler.nextInt();
+    }
+}
--- src/aula11/PessoaApp.java
+++ src/aula11/PessoaApp.java
@@ -2,9 +2,32 @@ package aula11;
 
 public class PessoaApp {
     public static void main(String[] args){
-        Pessoa pessoa = new Pessoa();
-        PessoaFisica pf = new PessoaFisica();
-        PessoaJuridica pj = new PessoaJuridica();
-        Object obj = new Object();
+        PessoaApp pessoa = new PessoaApp();
+        MenuApp menu = new MenuApp();
+        int opcao = 0;
+        while (opcao != 9) {
+            opcao = menu.getOpcao();
+            switch (opcao) {
+                case 1:pessoa.incluir();break;
+                case 2:pessoa.consultar();break;
+                case 3:pessoa.alterar();break;
+                case 4:pessoa.excluir();break;
+                case 9: break;
+                default: System.out.println("Opcao Invalida!");
+            }
+        }
+    }
+
+    private void incluir() {
+        System.out.println("Incluir");
+    }
+    private void consultar() {
+        System.out.println("Consultar");
+    }
+    private void alterar() {
+        System.out.println("Alterar");
+    }
+    private void excluir() {
+        System.out.println("Excluir");
     }
 }
