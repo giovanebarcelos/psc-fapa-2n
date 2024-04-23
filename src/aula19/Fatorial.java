38b014438f5fdda71744f93e7c06c7cbccd316b9
Giovane Barcelos <giovanebarcelos@gmail.com>
2024-04-22 21:05:02 -0300

    feat: calculo fatorial com leitura de números enquanto maior que zero

---
+++ src/aula19/Fatorial.java
@@ -0,0 +1,33 @@
+package aula19;
+
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        do {
+            System.out.print("Numero: ");
+            numero = scanner.nextInt();
+            if (numero > 0){
+                int numFatorial = fatorial(numero);
+                System.out.printf(
+                        "Fatorial de %d eh %d\n",
+                        numero, numFatorial);
+            }
+        } while (numero > 0);
+
+
+
+    }
+
+    public static int fatorial(int numero){
+        int fator = 1, fatorial = 1;
+        while (fator <= numero){
+            fatorial = fatorial * fator;
+            fator++;
+        }
+        return fatorial;
+    }
+}
